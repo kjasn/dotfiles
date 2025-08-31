2cf6ee1b4370a297ea9784c0cb4b49e5be64fb70
Kjasn <iz.kjasn@icloud.com>
2025-08-31 23:09:46 +0800

    enhance: minifiles lazy load

--- nvim/lua/plugins/minifiles.lua
+++ nvim/lua/plugins/minifiles.lua
@@ -1,6 +1,14 @@
 return {
 	"echasnovski/mini.files",
 	version = "*",
+	lazy = true,
+	keys = {
+		{
+			"<leader>e",
+			":lua MiniFiles.open(vim.api.nvim_buf_get_name(0))<cr>:lua MiniFiles.reveal_cwd()<cr>",
+			{ desc = "MiniFiles" },
+		},
+	},
 	config = function()
 		require("mini.files").setup()
 	end,
