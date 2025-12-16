dc2c2fea46a1433598141be93709535e1a5b336b
Kjasn <iz.kjasn@icloud.com>
2025-12-16 17:20:11 +0800

    feat: use catppuccin theme

---
+++ nvim/lua/plugins/colorschema.lua
@@ -0,0 +1,14 @@
+return {
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "catppuccin",
+		},
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		lazy = true,
+		priority = 1000,
+	},
+}
