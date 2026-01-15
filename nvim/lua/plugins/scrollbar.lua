eeb5b1da49a931fe78f436790ffbddd2a02b001a
Kjasn <iz.kjasn@icloud.com>
2026-01-15 11:22:14 +0800

    feat: add scroll view

---
+++ nvim/lua/plugins/scrollbar.lua
@@ -0,0 +1,37 @@
+return {
+	"petertriho/nvim-scrollbar",
+	dependencies = {
+		"lewis6991/gitsigns.nvim",
+		"kevinhwang91/nvim-hlslens",
+	},
+	opts = {},
+	config = function()
+		require("scrollbar").setup({
+			handle = {
+				text = " ",
+				color = "#C7E9E4",
+			},
+		})
+		require("gitsigns").setup()
+		require("hlslens").setup({
+			build_position_cb = function(plist, _, _, _)
+				require("scrollbar.handlers.search").handler.show(plist.start_pos)
+			end,
+		})
+		require("scrollbar.handlers.gitsigns").setup()
+		require("scrollbar.handlers.search").setup()
+	end,
+}
+-- return {
+-- 	"petertriho/nvim-scrollbar",
+-- 	dependencies = {
+-- 		"lewis6991/gitsigns.nvim",
+-- 		"kevinhwang91/nvim-hlslens",
+-- 	},
+-- 	opts = {
+-- 		handlers = {
+-- 			gitsigns = true,
+-- 			search = true,
+-- 		},
+-- 	},
+-- }
