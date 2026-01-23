484abe6688288b609d188a9d5b87e08f248ca658
Kjasn <iz.kjasn@icloud.com>
2026-01-23 22:01:53 +0800

    fix: update plugins

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -10,7 +10,7 @@ vim.api.nvim_create_autocmd("BufLeave", {
 	pattern = "*",
 	nested = true,
 	callback = function()
-		if vim.bo.modified then
+		if vim.bo.modified and vim.bo.buftype == "" and vim.fn.expand("%") ~= "" then
 			vim.cmd("silent! update")
 		end
 	end,
