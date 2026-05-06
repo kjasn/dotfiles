9d0ec718a2dcba6df4c82a0c715c6b6ef13f128f
kjasn <iz.kjasn@icloud.com>
2026-05-06 15:02:22 +0800

    fix(nvim): update nvim plugins config

--- nvim/lua/plugins/scrollbar.lua
+++ nvim/lua/plugins/scrollbar.lua
@@ -2,23 +2,25 @@ return {
 	"petertriho/nvim-scrollbar",
 	dependencies = {
 		"lewis6991/gitsigns.nvim",
-		"kevinhwang91/nvim-hlslens",
-	},
-	opts = {},
-	config = function()
-		require("scrollbar").setup({
-			handle = {
-				text = " ",
-				color = "#C7E9E4",
-			},
-		})
-		require("gitsigns").setup()
-		require("hlslens").setup({
-			build_position_cb = function(plist, _, _, _)
-				require("scrollbar.handlers.search").handler.show(plist.start_pos)
+		{
+			"kevinhwang91/nvim-hlslens",
+			config = function()
+				require("hlslens").setup({
+					build_position_cb = function(plist, _, _, _)
+						require("scrollbar.handlers.search").handler.show(plist.start_pos)
+					end,
+				})
 			end,
-		})
-		require("scrollbar.handlers.gitsigns").setup()
-		require("scrollbar.handlers.search").setup()
-	end,
+		},
+	},
+	opts = {
+		handle = {
+			text = " ",
+			color = "#C7E9E4",
+		},
+		handlers = {
+			gitsigns = true,
+			search = true,
+		},
+	},
 }
