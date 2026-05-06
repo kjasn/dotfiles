b0059d39656948f694b95a1f3f6d20dac561b08c
kjasn <iz.kjasn@icloud.com>
2026-05-06 15:03:37 +0800

    fix(nvim): disable auto save

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -2,30 +2,20 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-local augroup = vim.api.nvim_create_augroup("UserAutoCmds", { clear = true })
+-- local augroup = vim.api.nvim_create_augroup("UserAutoCmds", { clear = true })
 
--- Auto save when buffer is hidden (like VSCode onFocusChange)
-vim.api.nvim_create_autocmd("BufLeave", {
-	group = augroup,
-	pattern = "*",
-	nested = true,
-	callback = function()
-		if vim.bo.modified and vim.bo.buftype == "" and vim.fn.expand("%") ~= "" then
-			vim.cmd("silent! update")
-		end
-	end,
-	desc = "Auto save on buffer leave",
-})
-
--- Fallback: auto save on focus lost
-vim.api.nvim_create_autocmd("FocusLost", {
-	group = augroup,
-	pattern = "*",
-	nested = true,
-	callback = function()
-		if vim.bo.modified then
-			vim.cmd("silent! update")
-		end
-	end,
-	desc = "Auto save on focus lost",
-})
+-- Auto save when switching buffers (skip on exit)
+-- vim.api.nvim_create_autocmd("BufLeave", {
+-- 	group = augroup,
+-- 	pattern = "*",
+-- 	nested = true,
+-- 	callback = function()
+-- 		if vim.v.exiting ~= 0 then
+-- 			return
+-- 		end
+-- 		if vim.bo.modified and vim.bo.buftype == "" and vim.fn.expand("%") ~= "" then
+-- 			vim.cmd("silent! update")
+-- 		end
+-- 	end,
+-- 	desc = "Auto save on buffer leave (not on exit)",
+-- })
