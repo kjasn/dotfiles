b2255d9fd8d00ae30e08eddb14aa204be5dbd338
kjasn <iz.kjasn@icloud.com>
2026-05-19 15:44:29 +0800

    fix(nvim): update color sheme config to match nvim 0.12 🥲

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -2,39 +2,21 @@ return {
 	{
 		"LazyVim/LazyVim",
 		opts = {
-			colorscheme = "catppuccin",
+			colorscheme = "catppuccin-mocha",
 		},
 	},
 	{
 		"catppuccin/nvim",
-		lazy = true,
-		name = "catppuccin",
-		opts = {
-			custom_highlights = function(colors)
+		opts = function(_, opts)
+			opts.flavour = "mocha"
+			opts.integrations = opts.integrations or {}
+			opts.integrations.mini = true
+
+			opts.custom_highlights = function(colors)
 				return {
-					NvimWebDevicons = { fg = colors.lavender },
 					WinSeparator = { fg = colors.lavender },
 				}
-			end,
-			lsp_styles = {
-				underlines = {
-					errors = { "undercurl" },
-					hints = { "undercurl" },
-					warnings = { "undercurl" },
-					information = { "undercurl" },
-				},
-			},
-		},
-		specs = {
-			{
-				"akinsho/bufferline.nvim",
-				optional = true,
-				opts = function(_, opts)
-					if (vim.g.colors_name or ""):find("catppuccin") then
-						opts.highlights = require("catppuccin.special.bufferline").get_theme()
-					end
-				end,
-			},
-		},
+			end
+		end,
 	},
 }
